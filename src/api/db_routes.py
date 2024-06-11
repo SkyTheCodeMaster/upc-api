c183ea86e8d0422214d6d415529a996dd50029e8
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2024-06-10 21:46:39 -0400

    fix cors

--- src/api/db_routes.py
+++ src/api/db_routes.py
@@ -93,4 +93,4 @@ async def setup(app: web.Application) -> None:
   for route in routes:
     app.LOG.info(f"  ↳ {route}")
   app.add_routes(routes)
-  add_cors_routes()
+  add_cors_routes(routes, app)
